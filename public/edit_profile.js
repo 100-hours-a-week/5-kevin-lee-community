a68e81b613a879917740a61e1b48b2b7f168dd54
lky3004me <lky3004me@gmail.com>
2024-04-23 17:48:14 +0900

    twopage_completed_yeeeha

--- public/edit_profile.js
+++ public/edit_profile.js
@@ -1,15 +1,45 @@
-let dropdown = document.querySelector('.dropdown');
+document.getElementById('correction').addEventListener('click', function(){
+    correction();
+})
+document.getElementById('withdrawl').addEventListener('click', function(){
+    withdrawl();
+})
+document.getElementById('modal_cancel').addEventListener('click', function(){
+    closeModal();
+})
+document.getElementById('modal_confirm').addEventListener('click', function(){
+    confirmed();
+})
+const modal = document.getElementById('modal_container');
 
-dropdown.addEventListener('mouseenter', function(event) {
-    let target = event.target;
-    if (target.classList.contains('dropdown_dc')) {
-        target.style.backgroundColor = '#e9e9e9'; // 호버 시 배경색 변경
-    }
-});
+function correction(){
+    let nickname = document.getElementById('nickname_input').value;
+    let helper_text = document.getElementById('helper_text');
 
-dropdown.addEventListener('mouseleave', function(event) {
-    let target = event.target;
-    if (target.classList.contains('dropdown_dc')) {
-        target.style.backgroundColor = '#gainsboro'; // 마우스 나갈 때 배경색 원래대로 복구
+    if(nickname.length == 0){
+        helper_text.innerHTML = "*닉네임을 입력해주세요"
     }
-});
+    else if(nickname.length >= 11){
+        helper_text.innerHTML = "*닉네임은 최대 10자 까지 작성 가능합니다."
+    }
+    //중복 검사 코드 추가 필요
+    else{
+        let tostMessage = document.getElementById('tost_message');
+        tostMessage.classList.add('active');
+        setTimeout(function(){
+            tostMessage.classList.remove('active');
+        },1000);
+    }
+}
+
+function withdrawl(){
+    modal.classList.remove('hidden');
+}
+
+function closeModal(){
+    modal.classList.add('hidden');
+}
+function confirmed(){
+    window.location.href="/login"
+}
+
